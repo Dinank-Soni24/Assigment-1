bd5f263ecb54897006b4735bb31e8bebee35c37f
Zignuts Technolab <ztlab13@Zignutss-Mac-mini.local>
2023-02-23 11:43:21 +0530

    update indexfile

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,29 @@ $("#add_course").submit(function(event){
     alert("Data Inserted Successfully!");
 })
 
+$("#update_course").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3000/api/course/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully!");
+    })
+
+})
+
 
 if(window.location.pathname == "/"){
     $ondelete = $(".table tbody td a.delete");
